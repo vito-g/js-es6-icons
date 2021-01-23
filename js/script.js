d9ce4677deccdc888095222a3a9369b84e35b44d
Vito Gentile <gentilevito1076@gmail.com>
2021-01-23 15:15:17 +0100

    START

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*Milestone 1:
+- Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+- Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
+
+Milestone 2:
+- Definire un array di colori e associare ad ogni tipo di icona un colore.
+- Visualizzare le icone di colore diverso in base al tipo.
+
+Milestone 3:
+- Aggiungere una select per filtrare le icone in base al tipo.
+- Popolare le options della select dinamicamente e, ogni volta che cambia il valore selezionato, visualizzare le icone corrispondenti.
+
+FACCIAMOLO PRIMA BRUTTISSIMO.
+Dovete stupirvi per quanto sarà brutto. (e anche provare qualche brivido)
+E poi nel caso, se ci avanza tempo, allora abbelliamolo come negli screen che vi sto inviando.
+
+Consigli:
+Ragionate per singola milestone.
+Leggetevi prima il tutto giusto per avere chiara l'idea sulla nostra applicazione, ma poi stop.
+Prendete quindi solo la prima milestone e conquistatela senza più neanche pensare alle successive. E poi caffettino, e passate alla seconda.
+Per la terza milestone un po' di codice l'abbiamo già visto in classe. Approcciate con curiosità e creatività il tutto.*/
